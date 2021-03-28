f8f95d5cce1f4c53700dbbcab0bdc097b9da54d0
Prakhar317 <73430253+Prakhar317@users.noreply.github.com>
2021-03-28 11:54:29 +0530

    Update schemaScript.sql

--- schemaScript.sql
+++ schemaScript.sql
@@ -27,9 +27,69 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Resident` (
   `Occupation` VARCHAR(45) NULL,
   `Gender` VARCHAR(1) NOT NULL,
   PRIMARY KEY (`AccountNumber`))
-ENGINE = InnoDB;
-
-
+ENGINE = InnoDB;               
+               
+INSERT INTO `mydb`.`Resident`(
+	`AccountNumber`,
+    `FirstName`,
+    `LastName`,
+    `Tower`,
+    `HouseNumber`,
+    `Floor`,
+    `Occupation`,
+    `Gender`
+)
+VALUES(
+	1,
+    "Arjun",
+    "Singh",
+    "A",
+    202,
+    2,
+    "Civil Engineer",
+    "M"
+),
+(
+	2,
+    "Prateek",
+    "Kumar",
+    "A",
+    201,
+    2,
+    "Singer",
+    "M"
+),
+(
+	3,
+    "Jyoti",
+    "Agarwal",
+    "D",
+    502,
+    5,
+    "Chef",
+    "F"
+),
+(
+	4,
+    "Prashant",
+    "Singh",
+    "C",
+    908,
+    9,
+    "Mechanical Engineer",
+    "M"
+),
+(
+	5,
+    "Lavanya",
+    "Mishra",
+    "B",
+    202,
+    3,
+    "Anlayst",
+    "F"
+);
+               
 -- -----------------------------------------------------
 -- Table `mydb`.`PhoneNumbers`
 -- -----------------------------------------------------
@@ -40,6 +100,35 @@ CREATE TABLE IF NOT EXISTS `mydb`.`PhoneNumbers` (
   PRIMARY KEY (`PhoneNumber`))
 ENGINE = InnoDB;
 
+INSERT INTO `mydb`.`PhoneNumbers` (
+	`AccountNumber`,
+    `PhoneNumber`,
+    `UserType`
+)
+
+VALUES 
+(1, 9812678912, "resident"),
+(2, 9128120932, "resident"),
+(3, 9123121231, "resident"),
+(1, 9119191991, "visitor"),
+(2, 9111911999, "visitor"),
+(4, 9120293013, "resident"),
+(5, 8789389382, "resident"),
+(1, 9812278912, "serviceProvider"),
+(2, 9121110932, "serviceProvider"),
+(3, 8812121231, "serviceProvider"),
+(4, 9812371112, "serviceProvider"),
+(1, 9919291921, "maintenanceStaff"),
+(2, 9123391921, "maintenanceStaff"),
+(3, 9919211231, "maintenanceStaff"),
+(4, 8801291121, "maintenanceStaff"),
+(5, 9891929321, "maintenanceStaff"),
+(6, 8111291922, "maintenanceStaff"),
+(1, 9919291921, "securityStaff"),
+(2, 8352969221, "securityStaff"),
+(3, 9919293331, "securityStaff"),
+(4, 7799919121, "securityStaff"),
+(5, 9797291721, "securityStaff");
 
 -- -----------------------------------------------------
 -- Table `mydb`.`LicenseNumber`
@@ -51,7 +140,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`LicenseNumber` (
   PRIMARY KEY (`AccountNumber`, `LicenseNumber`))
 ENGINE = InnoDB;
 
-
+INSERT INTO `mydb`.`LicenseNumber`(
+`AccountNumber`,
+`LicenseNumber`,
+`UserType`
+)
+VALUES
+(1,9901,"visitor"),
+(1,9182,"resident"),
+(2,1923,"resident"),
+(3,1231,"resident"),
+(4,2323,"resident"),
+(5,2123,"resident"),
+(2,2314,"serviceProvider"),
+(4,1111,"serviceProvider");
+               
 -- -----------------------------------------------------
 -- Table `mydb`.`ServiceProvider`
 -- -----------------------------------------------------
@@ -63,7 +166,17 @@ CREATE TABLE IF NOT EXISTS `mydb`.`ServiceProvider` (
   PRIMARY KEY (`AccountNumber`))
 ENGINE = InnoDB;
 
-
+INSERT INTO `mydb`.`ServiceProvider` (
+`AccountNumber`,
+`AttendanceInformation`,
+`FirstName`,
+`Lastname`)
+VALUES
+(1,"APPPAPPPPPPAPPPPPPPPPPPPAPPAPP","Lakshay","Kumar"),
+(2,"PPPPPPPPAAAPPPPPPPPAPPPPPPPAPA","Pradeep","Singh"),
+(3,"PAPPPAAPPPPPPPPPPPPPAPPPPPAAAA","Sumit","Gupta"),
+(4,"PPPPPPAPPPPPPAPPPPPPAPPPPPPAAP","Dheeraj","Bhatia");
+               
 -- -----------------------------------------------------
 -- Table `mydb`.`Parking`
 -- -----------------------------------------------------
@@ -90,6 +203,23 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Parking` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+INSERT INTO `mydb`.`Parking` (
+`ParkingID`,
+`VehicleType`,
+`BasementLevel`,
+`ParkingNumber`,
+`UserType`,
+`Resident_AccountNumber`,
+`ServiceProvider_AccountNumber`)
+
+VALUES
+(1,"car",1,23,"resident",1,NULL),
+(2,"car",2,11,"resident",2,NULL),
+(3,"car",1,12,"resident",3,NULL),
+(4,"car",1,13,"resident",4,NULL),
+(5,"car",1,14,"resident",5,NULL),
+(6,"car",2,1,"serviceProvider",NULL,2),
+(7,"bike",1,2,"serviceProvider",NULL,4);
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Visitor`
@@ -103,7 +233,17 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Visitor` (
   PRIMARY KEY (`TempAccountNumber`))
 ENGINE = InnoDB;
 
-
+INSERT INTO `mydb`.`Visitor` (
+`TempAccountNumber`,
+`AccountNumber`,
+`FirstName`,
+`LastName`,
+`AccountExpiryTimeStamp`,
+`AccountStartTimeStamp`)
+VALUES
+(1,"Sushant", "Singh", '2020-04-02','2020-03-18'),
+(2,"Ritvik", "Thakur", '2020-03-29','2020-03-25');
+               
 -- -----------------------------------------------------
 -- Table `mydb`.`Complaint`
 -- -----------------------------------------------------
@@ -137,6 +277,20 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Complaint` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+INSERT INTO `mydb`.`Complaint`(
+`ComplaintID`,
+`TimeStamp`,
+`StaffResolutionStatus`,
+`ComplaintResolutionStatus`,
+`Description`,
+`Resident_AccountNumber`,
+`ServiceProvider_AccountNumber`,
+`Visitor_TempAccountNumber`,
+`Visitor_AccountNumber`)
+VALUES 
+(1,'2021-03-25 11:23:00','F','F','Leaking faucet',2,NULL,NULL,NULL,NULL),
+(2,'2021-03-23 18:20:00','F','F','Jammed door on 9th floor',4,NULL,NULL,NULL,NULL),
+(3,'2021-04-15 18:20:00','T','T','Parking spot occupied by unknown car',NULL,2,NULL,NULL);
 
 -- -----------------------------------------------------
 -- Table `mydb`.`TargetUser`
@@ -148,7 +302,13 @@ CREATE TABLE IF NOT EXISTS `mydb`.`TargetUser` (
   PRIMARY KEY (`ID`, `TargetUser`))
 ENGINE = InnoDB;
 
-
+INSERT INTO `mydb`.`TargetUser` (
+`ID`,
+`TargetUser`,
+`QueryType`)
+VALUES
+(1,"resident","announcement");
+               
 -- -----------------------------------------------------
 -- Table `mydb`.`MaintenanceStaff`
 -- -----------------------------------------------------
@@ -160,7 +320,19 @@ CREATE TABLE IF NOT EXISTS `mydb`.`MaintenanceStaff` (
   PRIMARY KEY (`AccountID`))
 ENGINE = InnoDB;
 
-
+INSERT INTO `mydb`.`MaintenanceStaff` (
+`AccountID`,
+`PhoneNumber`,
+`FirstName`,
+`LastName`)
+VALUES
+(1,9125789990,"Mrakhar","Paurya"),
+(2,7668219007,"Saurabh","Tiwari"),
+(3,8212839103,"Pankaj","Nag"),
+(4,9957773012,"Manish","Arora"),
+(5,9999229391,"Suraj","Singh"),
+(6,8878309203,"Jennifer","Lawerence");
+               
 -- -----------------------------------------------------
 -- Table `mydb`.`SecurityStaff`
 -- -----------------------------------------------------
@@ -172,7 +344,18 @@ CREATE TABLE IF NOT EXISTS `mydb`.`SecurityStaff` (
   PRIMARY KEY (`AccountID`))
 ENGINE = InnoDB;
 
-
+INSERT INTO `mydb`.`SecurityStaff` (
+`AccountID`,
+`AttendanceInformation`,
+`FirstName`,
+`LastName`)
+VALUES
+(1,"PPAPAPP","Rituraj","Gupta"),
+(2,"PPPPPPP","Ankit","Singh"),
+(3,"PPAAAAA","Rishi","Raj"),
+(4,"APPPPPP","Sakshi","Saxena"),
+(5,"PAPPAAP","Hariram","Singh");
+               
 -- -----------------------------------------------------
 -- Table `mydb`.`Announcement`
 -- -----------------------------------------------------
@@ -197,7 +380,17 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Announcement` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
+INSERT INTO `mydb`.`Announcement` (
+`AnnouncementID`,
+`TimeStamp`,
+`Description`,
+`MaintenanceStaff_AccountID`,
+`SecurityStaff_AccountID`)
+VALUES
+(1,'2021-03-12 13:00:00',"Club Closed today",2,NULL),
+(2,'2021-03-14 11:12:00',"Holi Celebration planning at 6 in the evening",1,NULL),
+(3,'2021-03-11 21:20:00',"Construction going on at Gate 3",NULL,2);
+              
 -- -----------------------------------------------------
 -- Table `mydb`.`Resident_has_ServiceProvider`
 -- -----------------------------------------------------
@@ -219,7 +412,18 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Resident_has_ServiceProvider` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+INSERT INTO `mydb`.`Resident_has_ServiceProvider`(
+`Resident_AccountNumber`,
+`ServiceProvider_AccountNumber`)
 
+VALUES
+(1,1),
+(2,3),
+(4,3),
+(3,1),
+(4,4),
+(5,2);
+               
 -- -----------------------------------------------------
 -- Table `mydb`.`Visitor_has_Resident`
 -- -----------------------------------------------------
@@ -241,6 +445,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Visitor_has_Resident` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+INSERT INTO `mydb`.`Visitor_has_Resident`(
+`Visitor_TempAccountNumber`,
+`Resident_AccountNumber`)
+VALUES
+(1,1),
+(2,3);
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
